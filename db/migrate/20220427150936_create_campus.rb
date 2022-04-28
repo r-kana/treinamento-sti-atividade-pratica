4f0cf87af5b4862b4f4672fea92c09363ade870b
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2022-04-28 19:36:34 +0000

    Criando migracao

--- db/migrate/20220427150936_create_campus.rb
+++ db/migrate/20220427150936_create_campus.rb
@@ -1,6 +1,12 @@
 class CreateCampus < ActiveRecord::Migration[6.1]
   def change
     create_table :campus do |t|
+      t.string :name
+      t.string :phone_number
+      t.string :address
+      t.string :cep 
+      t.string :city
+      t.string :neighborhood
 
       t.timestamps
     end
