4a04425b099b87ea9e605d70a15d9a0a9c2a9f6f
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2022-05-07 22:34:29 +0000

    Finalizando branch

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -1,16 +1,25 @@
 class AuthenticateController < ApplicationController
+  before_action :user_from_cookie
   def login
-    @user = User.find_by(iduff: params[:iduff])
-    @user = @user&.authenticate(params[:password])
+    @current_user = User.find_by(iduff: params[:iduff])
+    @current_user = @user&.authenticate(params[:password])
     respond_to do |format|
-      if @user
-        cookies.signed[:user_id] = @user.id
+      if @current_user
+        cookies.signed[:user_id] = @current_user.id
         format.html { redirect_to  home_url }
       else  
         format.html { redirect_to home_path, notice: 'Email ou senha incorretos'}
       end
     end
   end
+
+  def logout
+    cookies.delete :user_id
+    respond_to do |format|
+      format.html {redirect_to welcome_path}
+    end
+  end
+
 
 
 end
